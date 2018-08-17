caa7eb60d7e2bfacbbc0b932f0ba7b57e90c3e88
Matt Roberts <vtpanda@users.noreply.github.com>
2018-08-17 08:43:25 -0400

    Fixed ingestion.sh to call the correct python script.

--- etl/ingestion.sh
+++ etl/ingestion.sh
@@ -4,7 +4,7 @@ sudo pip install boto3
 sudo pip install watchtower
 if [ -e ${INPUT1_STAGING_DIR}/ingestion_parameters.json ]
 then
-    python ${INPUT1_STAGING_DIR}/ingestion_mydatalake.py "$(< ${INPUT1_STAGING_DIR}/ingestion_parameters.json)"
+    python ${INPUT1_STAGING_DIR}/ingestion.py "$(< ${INPUT1_STAGING_DIR}/ingestion_parameters.json)"
 else
-    python ${INPUT1_STAGING_DIR}/ingestion_mydatalake.py
+    python ${INPUT1_STAGING_DIR}/ingestion.py
 fi
