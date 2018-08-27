0d699aab045995df85c72cc5284dfe3def679975
Matt Roberts <vtpanda@users.noreply.github.com>
2018-08-27 11:43:24 -0400

    Added parameters to the shell scripts.

--- etl/Deployment/FileIngestion/deploy.sh
+++ etl/Deployment/FileIngestion/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+bucket=vtpanda-data-lake
+profile=myaws
+
 echo "Uploading files to S3"
 ./UploadFilesToS3.sh
 
 echo "Deploying SupportIAMRoles"
-aws cloudformation create-stack --stack-name SupportIAMRoles --template-url https://s3.amazonaws.com/vtpanda-data-lake/feeds/hmda/hmda_lar/scripts/deployment/SupportIAMRoles.yaml --capabilities CAPABILITY_NAMED_IAM --profile myaws
-aws cloudformation wait stack-create-complete --stack-name SupportIAMRoles --profile myaws
+aws cloudformation create-stack --stack-name SupportIAMRoles --template-url https://s3.amazonaws.com/$bucket/feeds/hmda/hmda_lar/scripts/deployment/SupportIAMRoles.yaml --capabilities CAPABILITY_NAMED_IAM --profile $profile
+aws cloudformation wait stack-create-complete --stack-name SupportIAMRoles --profile $profile
 if test "$?" != "0"
 then
     echo "SupportIAMRoles Did Not Return in a Timely Manner"
@@ -12,8 +15,8 @@ then
 fi
 
 echo "Deploying SupportLambdaFunctions"
-aws cloudformation create-stack --stack-name SupportLambdaFunctions --template-url https://s3.amazonaws.com/vtpanda-data-lake/feeds/hmda/hmda_lar/scripts/deployment/SupportLambdaFunctions.yaml --profile myaws
-aws cloudformation wait stack-create-complete --stack-name SupportLambdaFunctions --profile myaws
+aws cloudformation create-stack --stack-name SupportLambdaFunctions --template-url https://s3.amazonaws.com/$bucket/feeds/hmda/hmda_lar/scripts/deployment/SupportLambdaFunctions.yaml --profile $profile
+aws cloudformation wait stack-create-complete --stack-name SupportLambdaFunctions --profile $profile
 if test "$?" != "0"
 then
     echo "SupportLambdaFunctions Did Not Return in a Timely Manner"
@@ -21,8 +24,8 @@ then
 fi
 
 echo "Deploying SupportSNSTopics"
-aws cloudformation create-stack --stack-name SupportSNSTopics --template-url https://s3.amazonaws.com/vtpanda-data-lake/feeds/hmda/hmda_lar/scripts/deployment/SupportSNSTopics.yaml --profile myaws
-aws cloudformation wait stack-create-complete --stack-name SupportSNSTopics --profile myaws
+aws cloudformation create-stack --stack-name SupportSNSTopics --template-url https://s3.amazonaws.com/$bucket/feeds/hmda/hmda_lar/scripts/deployment/SupportSNSTopics.yaml --profile $profile
+aws cloudformation wait stack-create-complete --stack-name SupportSNSTopics --profile $profile
 if test "$?" != "0"
 then
     echo "SupportSNSTopics Did Not Return in a Timely Manner"
@@ -30,8 +33,8 @@ then
 fi
 
 echo "Deploying HMDADataIngestionDataPipelines"
-aws cloudformation create-stack --stack-name HMDADataIngestionDataPipelines --template-url https://s3.amazonaws.com/vtpanda-data-lake/feeds/hmda/hmda_lar/scripts/deployment/HMDAFileIngestionDataPipelines.yaml --parameters file://HMDAFileIngestionDataPipelines_CreateStack_Parameters.json --profile myaws
-aws cloudformation wait stack-create-complete --stack-name HMDADataIngestionDataPipelines --profile myaws
+aws cloudformation create-stack --stack-name HMDADataIngestionDataPipelines --template-url https://s3.amazonaws.com/$bucket/feeds/hmda/hmda_lar/scripts/deployment/HMDAFileIngestionDataPipelines.yaml --parameters file://HMDAFileIngestionDataPipelines_CreateStack_Parameters.json --profile $profile
+aws cloudformation wait stack-create-complete --stack-name HMDADataIngestionDataPipelines --profile $profile
 if test "$?" != "0"
 then
     echo "SupportSNSTopics Did Not Return in a Timely Manner"
