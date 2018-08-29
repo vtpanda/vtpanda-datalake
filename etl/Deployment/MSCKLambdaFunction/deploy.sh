3eea0b63945f3be64fe35501ba32f83782263f5a
Matt Roberts <vtpanda@users.noreply.github.com>
2018-08-29 11:04:15 -0400

    Added MSCK Lambda Function.

---
+++ etl/Deployment/MSCKLambdaFunction/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+profile=myaws
+
+echo "Uploading files to S3"
+aws s3 cp MSCKLambdaFunction.yaml s3://vtpanda-deployment/MSCKLambdaFunction/MSCKLambdaFunction.yaml --profile $profile
+
+echo "Deploying MSCKLambdaFunction"
+aws cloudformation create-stack --stack-name MSCKLambdaFunction --template-url https://s3.amazonaws.com/vtpanda-deployment/MSCKLambdaFunction/MSCKLambdaFunction.yaml --profile $profile
+aws cloudformation wait stack-create-complete --stack-name MSCKLambdaFunction --profile $profile
+if test "$?" != "0"
+then
+    echo "MSCKLambdaFunction Did Not Return in a Timely Manner"
+    exit $?
+fi
